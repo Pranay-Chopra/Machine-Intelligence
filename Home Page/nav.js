9cda7b412928f69e675aed2c32a843a0dbcf6c73
Pranay-Chopra <pranaychopra2007@gmail.co>
2021-12-15 08:59:16 +0530

    disable keybinds

--- Home Page/nav.js
+++ Home Page/nav.js
@@ -4,3 +4,24 @@ const container = document.querySelector(".container");
 hamburger_menu.addEventListener("click", () => {
     container.classList.toggle("active");
 });
+
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('dragstart', (e) => {
+  e.preventDefault()
+})
+
+document.addEventListener('drop', (e) => {
+  e.preventDefault()
+})
+document.addEventListener("keydown", function (event) {
+    if (event.ctrlKey) {
+        event.preventDefault();
+    }   
+});
+document.addEventListener("keydown", function (event) {
+  if (event.altKey) {
+      event.preventDefault();
+  }   
+});
+
+onselectstart = (e) => {e.preventDefault()}
